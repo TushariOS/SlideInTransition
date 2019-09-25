6965d31cda810a7b930c0b03eb299f5181ef332f
Tushar Jaunjalkar <tushar@heady.io>
2019-09-25 21:21:30 +0530

    Initial Commit

---
+++ SlideInTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideInTransition
+//
+//  Created by Tushar on 25/09/19.
+//  Copyright © 2019 Tushar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
